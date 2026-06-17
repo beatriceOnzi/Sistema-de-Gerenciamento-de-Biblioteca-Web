45ec6d645ef8fe5124446ce02653bb1283920314
beatrice <biaonca1200@gmail.com>
2026-06-17 15:13:05 -0300

    add table front

---
+++ src/static/js/tables.js
@@ -0,0 +1,38 @@
+var tabledata = [
+ 	{id:1, data_empre:"17-06-2026", aluno:"Oli Bob", livro:"O principe encantado e a princesa amaldiçoada", data_dev_prev:"24-06-2026"},
+ 	{id:2, data_empre:"17-06-2026", aluno:"Mary May", livro:"blue", data_dev_prev:"24-06-2026"},
+ 	{id:3, data_empre:"17-06-2026", aluno:"Christine Lobowski", livro:"green", data_dev_prev:"24-06-2026"},
+ 	{id:4, data_empre:"17-06-2026", aluno:"Brendon Philips", livro:"orange", data_dev_prev:"24-06-2026"},
+ 	{id:5, data_empre:"17-06-2026", aluno:"Margret Marmajuke", livro:"yellow", data_dev_prev:"24-06-2026"},
+ ];
+
+ //create Tabulator on DOM element with id "tabela_registro"
+var tabela_registro = new Tabulator("#tabela_registro", {
+ 	height: "100%",
+    data:tabledata, //assign data to table
+ 	layout:"fitColumns", //fit columns to width of table (optional)
+ 	columns:[ //Define Table Columns
+	 	{title:"Data de Empréstimo", field:"data_empre", hozAlign:"center", width:120, headerWordWrap:true, headerSort: false},
+	 	{title:"Aluno", field:"aluno", hozAlign:"left", formatter: "textarea", headerSort: false},
+	 	{title:"Livro", field:"livro", hozAlign:"left", formatter: "textarea", headerSort: false},
+	 	{title:"Data de Devolução", field:"data_dev_prev", hozAlign:"center", headerWordWrap:true, headerSort: false},
+ 	],
+});
+
+
+var tabela_atual = new Tabulator("#tabela_atual", {
+ 	height: "100%", // set height of table (in CSS or here), this enables the Virtual DOM and improves render speed dramatically (can be any valid css height value)
+ 	data:tabledata, //assign data to table
+ 	layout:"fitColumns", //fit columns to width of table (optional)
+ 	columns:[ //Define Table Columns
+	 	{title:"Data de Empréstimo", field:"data_empre", hozAlign:"center", width:120 ,headerWordWrap:true, headerSort: false},
+	 	{title:"Aluno", field:"aluno",hozAlign:"left", formatter: "textarea", headerSort: false},
+	 	{title:"Livro", field:"livro", hozAlign:"left", formatter: "textarea", headerSort: false},
+	 	{title:"Data de Devolução", field:"data_dev_prev", hozAlign:"center", headerWordWrap:true, headerSort: false},
+ 	],
+});
+
+//trigger an alert message when the row is clicked
+table.on("rowClick", function(e, row){ 
+	alert("Row " + row.getData().id + " Clicked!!!!");
+});
