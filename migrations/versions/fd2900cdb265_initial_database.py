597f4856a43f367e26622c97fe4001e4a99f1115
beatrice <biaonca1200@gmail.com>
2026-08-10 16:43:54 -0300

    migrate + waitress fix

---
+++ migrations/versions/fd2900cdb265_initial_database.py
@@ -0,0 +1,24 @@
+"""Initial database
+
+Revision ID: fd2900cdb265
+Revises: 
+Create Date: 2026-08-10 16:03:36.633296
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'fd2900cdb265'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
--- serve.py
+++ serve.py
@@ -5,10 +5,16 @@ from waitress import serve
 
 from app import app
 
+
 load_dotenv()
+
+host = os.getenv("HOST", "0.0.0.0")
+port = int(os.getenv("PORT", "5000"))
+
+print(f"Servidor iniciando em {host}:{port}")
 
 serve(
     app,
-    host=os.getenv("HOST", "0.0.0.0"),
-    port=int(os.getenv("PORT", 5000))
+    host=host,
+    port=port
 )
